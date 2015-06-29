6f6ab2c0c493f7c02001e713303e708dad77d30b
Giulio Ungaretti <giulioungaretti@me.com>
2015-06-29 11:21:40 +0200

    init scrpit

---
+++ init.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#
+# init.sh
+# Copyright (C) 2015 giulio <giulioungaretti@me.com>
+#
+# Distributed under terms of the MIT license.
+#
+echo EDITOR="vim" >> /home/$USER/.bashrc
+echo ./inputrc  > /home/$USER/.inputrc
